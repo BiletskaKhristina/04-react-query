bc4c08c9a85ba00258777705e1a522c1a0caa9ac
Білецька <khrista.biletcka7@gmail.com>
2026-06-25 13:33:00 +0300

    2

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "modern-normalize";
+import "modern-normalize/modern-normalize.css";
 
 const queryClient = new QueryClient();
 
